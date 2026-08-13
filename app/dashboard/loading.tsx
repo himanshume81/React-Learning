654a013938851907988c7dc966ddff13db2371fe
himanshume81 <himanshu.kumar@appinventiv.com>
2026-08-13 12:46:49 +0530

    user-management

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@/components/atoms/Spinner";
+
+export default function DashboardLoading() {
+  return (
+    <div className="flex flex-1 items-center justify-center py-24">
+      <Spinner />
+    </div>
+  );
+}
